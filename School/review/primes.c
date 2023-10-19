6d100f99dd45c1cdfc597a972175d2ee82ea8ab9
W45 <73011833+wgs45@users.noreply.github.com>
2023-10-19 20:19:25 +0800

    Add files via upload

--- School/review/primes.c
+++ School/review/primes.c
@@ -1,60 +1,53 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int is_prime(int n) {
-    if (n <= 1) {
-        return 0;
-    }
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            return 0;
-        }
-    }
-    return 1;
-}
-
-void find_prime_arithmetic_sequences(int N1, int N2, int LENGTH) {
-    int found = 0;
-
-    for (int i = N1; i <= N2 - LENGTH + 1; i++) {
-        for (int j = i + 1; j <= N2; j++) {
-            int d = j - i;
-            int prime = 1;
-            int current = i;
-
-            for (int k = 0; k < LENGTH; k++) {
-                if (!is_prime(current)) {
-                    prime = 0;
-                    break;
-                }
-                current += d;
-            }
-
-            if (prime) {
-                if (found == 0) {
-                    // ! do nothing
-                }
-                for (int k = 0; k < LENGTH; k++) {
-                    printf("%d", i + k * d);
-                    if (k < LENGTH - 1) {
-                        printf(",");
-                    }
-                }
-                printf("\n");
-                found = 1;
-            }
-        }
-    }
-
-    if (found == 0) {
-        printf("0\n");
-    }
-}
+// function prototypes
+bool isPrime(int num);
+void findPrimeArithmeticSequences(int N1, int N2, int LENGTH);
 
 int main() {
     int N1, N2, LENGTH;
-    printf("Enter N1, N2, LENGTH: ");
+    printf("Enter N1 N2 LENGTH (separated by whitespace): ");
     scanf("%d %d %d", &N1, &N2, &LENGTH);
-    find_prime_arithmetic_sequences(N1, N2, LENGTH);
+
+    findPrimeArithmeticSequences(N1, N2, LENGTH);
 
     return 0;
+}
+
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 == 0 || num % 3 == 0) return false;
+    for (int i = 5; i * i <= num; i += 6) {
+        if (num % i == 0 || num % (i + 2) == 0) return false;
+    }
+    return true;
+}
+
+void findPrimeArithmeticSequences(int N1, int N2, int LENGTH) {
+    for (int a = N1; a <= N2; a++) {
+        for (int b = a + 1; b <= N2; b++) {
+            if (isPrime(a) && isPrime(b)) {
+                int diff = b - a;
+                if (diff >= 0) {
+                    int current = b;
+                    int count = 2;
+                    while (count < LENGTH && current + diff <= N2 && isPrime(current + diff)) {
+                        current += diff;
+                        count++;
+                    }
+                    if (count == LENGTH) {
+                        current = b;
+                        printf("%d", a);
+                        for (int i = 0; i < LENGTH - 1; i++) {
+                            printf(",%d", current);
+                            current += diff;
+                        }
+                        printf("\n");
+                    }
+                }
+            }
+        }
+    }
 }
