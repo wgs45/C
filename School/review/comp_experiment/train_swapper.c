2d804e80f43fa01089eae172e3abddd488a17e9b
W45 <73011833+wgs45@users.noreply.github.com>
2023-11-18 18:59:03 +0800

    Update train_swapper.c

--- School/review/comp_experiment/train_swapper.c
+++ School/review/comp_experiment/train_swapper.c
@@ -1,45 +1,55 @@
+// TRAIN SWAPPING
+// L5
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    // Declare variables
-    int num, len, train[50] = {0};
-    int x, y, temp;
+// Declare global variables
+int n;
+int length;
+int train[60] = {0};
+int i, j, k;
+int temp;
+int count;
 
-    // Read the number of use cases
-    scanf("%d", &num);
+int main(){
+    // Read the number of test cases
+    printf("Enter the number of test cases: ");
+    scanf("%d", &n);
 
-    // num-- is used to store the number of use cases
-    // then it will loop over the number of use cases until it reaches 0
-    // then the program stops
-    while(num--) {
+    // Loop through each test case
+    while(n--){
+        count = 0;
+
         // Read the length of the train
-        scanf("%d", &len);
+        printf("Enter the length of the train: ");
+        scanf("%d", &length);
 
-        // Initialize a counter to keep track of the number of swaps
-        int count = 0;
-
-        // Read the train numbers
-        for (x = 0; x < len; x++) {
-            scanf("%d", &train[x]);
+        // Read the numbers of the train
+        printf("Enter the numbers of the train: ");
+        for(int i=0; i<length; i++){
+            scanf("%d", &train[i]);
         }
 
-        // Sort the train numbers using the bubble sort algorithm
-        for (x = 0; x < len; x++) {
-            for (y = 0; y < len - 1 - x; y++) {
-                // Compare adjacent numbers and swap if necessary
-                if (train[x] > train[y + 1]) {
-                    temp = train[x];
-                    train[x] = train[y + 1];
-                    train[y + 1] = temp;
+        // Sort the train in ascending order using bubble sort
+        for(int i=0; i<length; i++){
+            for(int j=0; j<length-1; j++){
+                if(train[j] > train[j+1]){
+                    printf("train[%d] > train[%d]\n", train[j], train[j+1]);
+                    // Swap the elements
+                    temp = train[j];
+                    printf("temp = %d\n", temp);
+                    train[j] = train[j+1];
+                    printf("train[j] = %d\n", train[j]);
+                    train[j+1] = train[j];
+                    printf("train[j+1] = %d\n", train[j+1]);
                     count++;
+                    printf("count = %d\n", count);
                 }
             }
         }
 
-        // Print the number of swaps needed for the optimal train swapping
-        printf("Optimal train swapping takes %d swaps.\n", count);
+        // Print the number of swaps required
+        printf("optimal train swapping takes %d swaps\n", count);
     }
-
-    // End of the program
     return 0;
 }
