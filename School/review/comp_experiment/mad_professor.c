e8cf3e164f4e99d33e14bccde75f8acec3d5d8f2
W45 <73011833+wgs45@users.noreply.github.com>
2023-12-17 20:36:42 +0800

    Add files via upload

--- School/review/comp_experiment/mad_professor.c
+++ School/review/comp_experiment/mad_professor.c
@@ -1,212 +1,37 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
 
-// c program to predict what a professor is saying
+// c program to predict what the mad professor is saying
 // source: uva10222
 
-// sample input:
-// k[r dyt I[o
+int main(){
+    // Initialize an array of characters with size 999 and set all elements to 0
+    char str[999] = {0};
 
-// sample output:
-// how are you
+    // Define two arrays of characters
+    // ct array represents the characters to be replaced
+    char ct[ ] = " 234567890-=ertyuiop[]\\dfghjkl;'cvbnm,./";
+    // pt array represents the characters to replace with
+    char pt[ ] = " `1234567890qwertyuiop[asdfghjklzxcvbnm,";
 
-int main() {
-    char c[100000];
-    int i, s;
+    int i, j;
 
-    while (gets(c))
-    {
-        for (i = 0; i < strlen(c); i++)
-        {
-            switch (c[i])
-            {
-            // capital letter
-            case 'B':
-                printf("c");
-                break;
-            case 'C':
-                printf("z");
-                break;
-            case 'D':
-                printf("a");
-                break;
-            case 'E':
-                printf("q");
-                break;
-            case 'F':
-                printf("s");
-                break;
-            case 'G':
-                printf("d");
-                break;
-            case 'H':
-                printf("f");
-                break;
-            case 'I':
-                printf("y");
-                break;
-            case 'J':
-                printf("g");
-                break;
-            case 'K':
-                printf("h");
-                break;
-            case 'L':
-                printf("j");
-                break;
-            case 'M':
-                printf("b");
-                break;
-            case 'N':
-                printf("v");
-                break;
-            case 'O':
-                printf("u");
-                break;
-            case 'P':
-                printf("i");
-                break;
-            case 'R':
-                printf("w");
-                break;
-            case 'T':
-                printf("e");
-                break;
-            case 'U':
-                printf("t");
-                break;
-            case 'V':
-                printf("x");
-                break;
-            case 'Y':
-                printf("r");
-                break;
+    // Get input from the user and store it in the str array
+    gets(str);
 
-            case '/':
-                printf(",");
-                break;
-            case ',':
-                printf("n");
-                break;
-            case 39:
-                printf("l");
-                break;
-            case ';':
-                printf("k");
-                break;
-            case ']':
-                printf("p");
-                break;
-            case '[':
-                printf("o");
-                break;
-            case ' ':
-                printf(" ");
-                break;
-            case '.':
-                printf("m");
-                break;
-            // small
-            case 'b':
-                printf("c");
-                break;
-            case 'c':
-                printf("z");
-                break;
-            case 'd':
-                printf("a");
-                break;
-            case 'e':
-                printf("q");
-                break;
-            case 'f':
-                printf("s");
-                break;
-            case 'g':
-                printf("d");
-                break;
-            case 'h':
-                printf("f");
-                break;
-            case 'i':
-                printf("y");
-                break;
-            case 'j':
-                printf("g");
-                break;
-            case 'k':
-                printf("h");
-                break;
-            case 'l':
-                printf("j");
-                break;
-            case 'm':
-                printf("b");
-                break;
-            case 'n':
-                printf("v");
-                break;
-            case 'o':
-                printf("u");
-                break;
-            case 'p':
-                printf("i");
-                break;
-            case 'r':
-                printf("w");
-                break;
-            case 't':
-                printf("e");
-                break;
-            case 'u':
-                printf("t");
-                break;
-            case 'v':
-                printf("x");
-                break;
-            case 'y':
-                printf("r");
-                break;
-
-            case '\\':
-                printf("[");
-                break;
-            case '=':
-                printf("0");
-                break;
-            case '-':
-                printf("9");
-                break;
-            case '0':
-                printf("8");
-                break;
-            case '9':
-                printf("7");
-                break;
-            case '8':
-                printf("6");
-                break;
-            case '7':
-                printf("5");
-                break;
-            case '6':
-                printf("4");
-                break;
-            case '5':
-                printf("3");
-                break;
-            case '4':
-                printf("2");
-                break;
-            case '3':
-                printf("1");
-                break;
-            case '2':
-                printf("`");
-                break;
+    // Iterate through each character in the str array
+    for(i = 0; i < strlen(str); i++) {
+        // Iterate through each character in the ct array
+        for(j = 0; j < strlen(ct); j++) {
+            // If the current character in the str array matches the current character in the ct array
+            if(str[i] == ct[j]) {
+                // Print the corresponding character from the pt array
+                printf("%c", pt[j]);
             }
         }
-        printf("\n");
     }
+    // Print a new line character
+    printf("\n");
+
     return 0;
 }
