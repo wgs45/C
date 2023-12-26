aa09a1c5bb5eac717f28276fe6cf82901cc434bf
W45 <73011833+wgs45@users.noreply.github.com>
2023-12-26 11:25:31 +0800

    Update tower_of_hanoi.c

--- School/review/final_exam_dec_2023/answer/tower_of_hanoi.c
+++ School/review/final_exam_dec_2023/answer/tower_of_hanoi.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 
-void towerOfHanoi(int n, char source, char auxiliary, char destination) {
-    if (n == 1) {
-        printf("Move disk 1 from %c to %c\n", source, destination);
-        return;
-    }
-
-    towerOfHanoi(n - 1, source, destination, auxiliary);
-    printf("Move disk %d from %c to %c\n", n, source, destination);
-    towerOfHanoi(n - 1, auxiliary, source, destination);
-}
+void towerOfHanoi(int n, char source, char auxiliary, char destination);
 
 int main() {
     int numDisks;
@@ -22,4 +13,15 @@ int main() {
     towerOfHanoi(numDisks, 'A', 'B', 'C');
 
     return 0;
+}
+
+void towerOfHanoi(int n, char source, char auxiliary, char destination) {
+    if (n == 1) {
+        printf("Move disk 1 from %c to %c\n", source, destination);
+        return;
+    }
+
+    towerOfHanoi(n - 1, source, destination, auxiliary);
+    printf("Move disk %d from %c to %c\n", n, source, destination);
+    towerOfHanoi(n - 1, auxiliary, source, destination);
 }
