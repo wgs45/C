12bb4d0235075d67a8cf5cf2f1763669ad9ec9a0
W45 <73011833+wgs45@users.noreply.github.com>
2023-12-26 11:24:49 +0800

    Create tower_of_hanoi.c

---
+++ School/review/final_exam_dec_2023/answer/tower_of_hanoi.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void towerOfHanoi(int n, char source, char auxiliary, char destination) {
+    if (n == 1) {
+        printf("Move disk 1 from %c to %c\n", source, destination);
+        return;
+    }
+
+    towerOfHanoi(n - 1, source, destination, auxiliary);
+    printf("Move disk %d from %c to %c\n", n, source, destination);
+    towerOfHanoi(n - 1, auxiliary, source, destination);
+}
+
+int main() {
+    int numDisks;
+
+    // Input the number of disks
+    printf("Enter the number of disks: ");
+    scanf("%d", &numDisks);
+
+    // Perform Tower of Hanoi
+    towerOfHanoi(numDisks, 'A', 'B', 'C');
+
+    return 0;
+}
