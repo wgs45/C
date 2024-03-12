17617447693c373d776a148dd12f0ffa7e7d8b15
W45 <73011833+wgs45@users.noreply.github.com>
2024-03-12 21:14:54 +0800

    Add files via upload

--- University/prog_1_semester_2_year_2024/exercise/fourth_week/star_1.c
+++ University/prog_1_semester_2_year_2024/exercise/fourth_week/star_1.c
@@ -1,43 +1,57 @@
 #include <stdio.h>
 
 int main() {
+	int k=3, h= 0, p = 0, f = 3, q = 3, y = 0;
+	int i, j, g;
 
-    for (int i = 1; i <= 4; ++i) {
-        for (int j = 1; j <= i; ++j) {
-            printf("*");
-        }
-        printf("\n");
-    }
+	for(i = 0;i < 4; i++){
+		for(j = k; j < 4; j++){
+			printf("*");
+		}
+		k -= 1;
+		printf("\n");
+	}
 
-    for (int i = 4; i >= 1; --i) {
-        for (int j = 4; j > i; --j) {
-            printf(" ");
-        }
-        for (int k = 1; k <= i; ++k) {
-            printf("*");
-        }
-        printf("\n");
-    }
+	printf("\n");
 
-    for (int i = 1; i <= 4; ++i) {
-        for (int j = 2; j <= i; ++j) {
-            printf(" ");
-        }
-        for (int k = 4; k >= i; --k) {
-            printf("*");
-        }
-        printf("\n");
-    }
+	for(i = 0;i < 4; i++){
+			for(j = h;j < 4; j++){
+			printf("*");
+		}
+			h += 1;
+			printf("\n");
+	}
 
-    for (int i = 4; i >= 1; --i) {
-        for (int j = 4; j > i; --j) {
-            printf(" ");
-        }
-        for (int k = 1; k <= i; ++k) {
-            printf("*");
-        }
-        printf("\n");
-    }
+	printf("\n");
 
-    return 0;
+	for(i = 0; i < 4; i++){
+			for(j = p; j < 4; j++){
+			printf("*");
+		}
+			f -= 1;
+			p += 1;
+			printf("\n");
+			for(g = f; g < 3; g++){
+				printf(" ");
+			}
+	}
+
+	printf("\n");
+
+	printf("   ");
+
+	for(i = 0; i < 4; i++){
+		for(j = q; j < 4; j++){
+			printf("*");
+		}
+		y += 1;
+		q -= 1;
+		printf("\n");
+		for(g = y; g < 3 ; g++){
+			printf(" ");
+		}
+	}
+
+
+	return 0;
 }
