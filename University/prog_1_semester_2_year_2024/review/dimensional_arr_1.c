24df26bf340f02d86f596982ca38d8a13e2cc6ef
W45 <73011833+wgs45@users.noreply.github.com>
2024-03-26 21:52:09 +0800

    Add files via upload

---
+++ University/prog_1_semester_2_year_2024/review/dimensional_arr_1.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int main() {
+    int directions = 0;
+    int matrix[100][100];
+    int x = 0, y = 0, i = 1, n = 4;
+
+    for (y = 0; y < n; y++) {
+        for (x = 0; x < n; x++) {
+            matrix[y][x] = 0;
+        }
+    }
+
+    x = 0; y = 0;
+
+    while(i <= 16) {
+        matrix[y][x] = i;
+
+        // Check if matrix can go with the current directions
+        // if can't, rotate 90 deg
+
+        if (directions == 0) {
+            if (x + 1 >= n || matrix[y][x + 1] != 0) {
+                directions = 1;
+            } 
+        } else if (directions == 1) {
+            if (y + 1 >= n || matrix[y + 1][x] != 0) {
+                directions = 2;
+            }
+        } else if (directions == 2) {
+            if (x - 1 < 0 || matrix[y][x - 1] != 0) {
+                directions = 3;
+            }
+        } else if (directions == 3) {
+            if (y - 1 < 0 || matrix[y - 1][x] != 0) {
+                directions = 0;
+            }
+        }
+
+        // go with specified directions
+        if (directions == 0) {
+            x++;
+        } else if (directions == 1) {
+            y++;
+        } else if (directions == 2) {
+            x--;
+        } else if (directions == 3) {
+            y--;
+        }
+        i++;
+    }
+
+    for (y = 0; y < n; y++) {
+        for (x = 0; x < n; x++) {
+            printf("%2d ", matrix[y][x]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
