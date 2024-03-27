857601d1c946a16a4bfad004c51506d892d7dbe4
W45 <73011833+wgs45@users.noreply.github.com>
2024-03-27 12:39:00 +0800

    Update guessing_number.c

--- University/prog_1_semester_2_year_2024/exercise/second_week/guessing_number.c
+++ University/prog_1_semester_2_year_2024/exercise/second_week/guessing_number.c
@@ -3,35 +3,35 @@
 #include <time.h>
 
 int main() {
-    // user variable
-    int user_guess = 0;
-    int temp_i = 0;
-    int temp_j = 0;
-    int initial_temp_i, initial_temp_j;
+    int guess_number = 0;
+    int i = 0, j = 0;
+    int temp_i = 0, temp_j = 0, initial_i = 0, initial_j = 0;
 
-    // number generator from 1 to 100
+    // number generator
     srand(time(0));
     int random_number = (rand() % 100) + 1;
-    printf("random: %d\n", random_number);
+    printf("Random numbers: %d\n", random_number);
 
-    // guessing
-    while (1) {
-        printf("Guess a number from 1 to 100: ");
-        scanf("%d", &user_guess);
-        if (user_guess == random_number) {
-            printf("You are correct");
+    while(1) {
+        printf("Please guess a number from 0 - 100: ");
+        scanf("%d", &guess_number);
+
+        if (guess_number > 100) {
+            printf("Invalid value, please guess from 1 to 100.\n");
+        } else if (guess_number == random_number) {
+            printf("Your are correct!\n");
             break;
-        } else {
-            if (user_guess > random_number) {
-                temp_i = user_guess - 1;
-                initial_temp_i = random_number + 1;
-                printf("guess from %d to %d\n", initial_temp_i, temp_i);
-            } else if (user_guess < random_number) {
-                temp_j = user_guess + 1;
-                initial_temp_j = temp_j;
-                printf("guess from %d to %d\n", initial_temp_j, 100);
-            }
-        }
+        } else if (guess_number < random_number) {
+            temp_i = guess_number - 1;
+            initial_i = random_number + 1;
+            printf("guess from %d to %d\n", initial_i, temp_i);
+        } else if (guess_number > random_number) {
+            temp_j = guess_number + 1;
+            initial_j = temp_j;
+            printf("guess from %d to %d\n", initial_j, 100);
+        } 
     }
+   
+
     return 0;
 }
