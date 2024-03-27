d8aa5204f9fd89d952b85d4397586b9ec802477b
W45 <73011833+wgs45@users.noreply.github.com>
2024-03-27 19:36:03 +0800

    Update guessing_number.c

--- University/prog_1_semester_2_year_2024/exercise/third_week/guessing_number.c
+++ University/prog_1_semester_2_year_2024/exercise/third_week/guessing_number.c
@@ -7,18 +7,22 @@ int main() {
     int randomarray[4];
     int random_number = 0;
 
+    // seed generator
     srand(time(NULL));
 
+    // random number generator
     for (int i = 0; i < 4; i++) {
-        int index = rand() % (10 - i);
-        randomarray[i] = digits[index];
+        int index = rand() % (10 - i); // generates a random index within the remaining numbers
 
-        digits[index] = digits[9 - i];
+        randomarray[i] = digits[index]; // stores the random selected digit in variable randomarray
+        
+        digits[index] = digits[9 - i]; // replace the selected digits with the last digits in variable digit array to avoid duplicatied digits in randomarray 
 
-        random_number = random_number * 10 + randomarray[i];
+        random_number = random_number * 10 + randomarray[i]; // Constructs the 4-digit random number.
+
     }
 
-    // printf("%d\n", random_number);
+    printf("random number: %d\n", random_number);
 
     while (1) {
         printf("Insert a number: ");
@@ -26,7 +30,8 @@ int main() {
         scanf("%d", &user_input);
 
         int user_array[4];
-
+    
+        // convert user inpu to array
         for (int i = 3; i >= 0; i--) {
             user_array[i] = user_input % 10;
             user_input /= 10;
@@ -34,6 +39,7 @@ int main() {
 
         int A = 0, B = 0;
 
+        // Calculate A (Correct Digits in Correct Positions) and B (Correct Digits in Wrong Positions)
         for (int i = 0; i < 4; i++) {
             if (user_array[i] == randomarray[i]) {
                 A++;
