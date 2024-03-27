b1ee192dc0d011d6d071d5ec070da147321f7637
W45 <73011833+wgs45@users.noreply.github.com>
2024-03-27 12:50:18 +0800

    Update guessing_number.c

--- University/prog_1_semester_2_year_2024/exercise/second_week/guessing_number.c
+++ University/prog_1_semester_2_year_2024/exercise/second_week/guessing_number.c
@@ -4,34 +4,35 @@
 
 int main() {
     int guess_number = 0;
-    int i = 0, j = 0;
-    int temp_i = 0, temp_j = 0, initial_i = 0, initial_j = 0;
+    int random_number;
+    int lower_bound = 1, upper_bound = 100;
 
-    // number generator
+    // Number generator
     srand(time(0));
-    int random_number = (rand() % 100) + 1;
-    printf("Random numbers: %d\n", random_number);
+    random_number = (rand() % 100) + 1;
+    printf("Random number: %d\n", random_number);
 
     while(1) {
-        printf("Please guess a number from 0 - 100: ");
+        printf("Please guess a number from 1 to 100: ");
         scanf("%d", &guess_number);
 
-        if (guess_number > 100) {
+        // Check for valid input
+        if (guess_number < 1 || guess_number > 100) {
             printf("Invalid value, please guess from 1 to 100.\n");
-        } else if (guess_number == random_number) {
-            printf("Your are correct!\n");
+            continue;
+        }
+
+        if (guess_number == random_number) {
+            printf("You are correct!\n");
             break;
         } else if (guess_number < random_number) {
-            temp_i = guess_number - 1;
-            initial_i = random_number + 1;
-            printf("guess from %d to %d\n", initial_i, temp_i);
-        } else if (guess_number > random_number) {
-            temp_j = guess_number + 1;
-            initial_j = temp_j;
-            printf("guess from %d to %d\n", initial_j, 100);
-        } 
+            printf("Guess from %d to %d\n", guess_number + 1, upper_bound);
+            lower_bound = guess_number + 1;
+        } else {
+            printf("Guess from %d to %d\n", lower_bound, guess_number - 1);
+            upper_bound = guess_number - 1;
+        }
     }
-   
 
     return 0;
 }
