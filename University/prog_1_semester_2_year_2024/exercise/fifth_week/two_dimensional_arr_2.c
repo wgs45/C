66facf3436d3951840b2464c592f8a21ea714bad
W45 <73011833+wgs45@users.noreply.github.com>
2024-03-28 19:12:43 +0800

    Update two_dimensional_arr_2.c

--- University/prog_1_semester_2_year_2024/exercise/fifth_week/two_dimensional_arr_2.c
+++ University/prog_1_semester_2_year_2024/exercise/fifth_week/two_dimensional_arr_2.c
@@ -1,48 +1,53 @@
 #include <stdio.h>
 
 int main() {
-    int N = 0;
-    scanf("%d", &N);
+    int matrix[4][4] = {0};  // Initialize matrix with zeros
+    int x = 0, y = 0, z = 1, n = 4;
+    int directions = 0;  // 0 - right, 1 - down, 2 - left, 3 - up
 
-    int matrix[N][N];
-    int top_row = 0, bottom_row = N - 1, left_column = 0, right_column = N - 1, num = 1;
+    while (z <= 16) {
+        matrix[y][x] = z;
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            matrix[i][j] = 0;
+        if (directions == 0) {
+            if (x + 1 >= n || matrix[y][x + 1] != 0) {
+                directions = 1;
+                y++;
+            } else {
+                x++;
+            }
+        } else if (directions == 1) {
+            if (y + 1 >= n || matrix[y + 1][x] != 0) {
+                directions = 2;
+                x--;
+            } else {
+                y++;
+            }
+        } else if (directions == 2) {
+            if (x - 1 < 0 || matrix[y][x - 1] != 0) {
+                directions = 3;
+                y--;
+            } else {
+                x--;
+            }
+        } else if (directions == 3) {
+            if (y - 1 < 0 || matrix[y - 1][x] != 0) {
+                directions = 0;
+                x++;
+            } else {
+                y--;
+            }
         }
-    }
- 
-    while (num <= N * N) {
-        // Fill top row
-        for (int i = left_column; i <= right_column; i++)
-            matrix[top_row][i] = num++;
-        top_row++;
 
-        // Fill right column
-        for (int i = top_row; i <= bottom_row; i++)
-            matrix[i][right_column] = num++;
-        right_column--;
-
-        // Fill bottom row
-        for (int i = right_column; i >= left_column; i--)
-            matrix[bottom_row][i] = num++;
-        bottom_row--;
-
-        // Fill left column
-        for (int i = bottom_row; i >= top_row; i--)
-            matrix[i][left_column] = num++;
-        left_column++;
+        z++;
     }
 
     // Print the matrix
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("%2d ", matrix[i][j]);
+    for (y = 0; y < n; y++) {
+        for (x = 0; x < n; x++) {
+            printf("%2d ", matrix[y][x]);
         }
         printf("\n");
     }
 
     return 0;
 }
-
