147962de6570d669756600b948a6b864b1176caf
W45 <73011833+wgs45@users.noreply.github.com>
2024-04-16 07:02:54 +0800

    Add files via upload

--- University/prog_1_semester_2_year_2024/exercise/eight_week/prime_num.c
+++ University/prog_1_semester_2_year_2024/exercise/eight_week/prime_num.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+bool is_prime(int num);
 
 int main() {
+    int num = 0, i = 0;
 
+    printf("Enter a value: ");
+    scanf("%d", &num);
+
+    printf("Prime numbers that are less than %d are: \n", num);
+    for (i = 2; i < num; i++) {
+        if (is_prime(i)) {
+            printf("%d ", i);
+        }
+    }
+ 
     return 0;
+}
+
+bool is_prime(int num) {
+    int i;
+
+    if (num <= 1) {
+        return false;
+    }
+
+    for (i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
--- University/prog_1_semester_2_year_2024/exercise/eight_week/vending_machine.c
+++ University/prog_1_semester_2_year_2024/exercise/eight_week/vending_machine.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 
+void vending(int amount);
+
 int main() {
-    
+    int amount = 0;
+
+    printf("Enter amount: ");
+    scanf("%d", &amount);
+
+    vending(amount);
+
     return 0;
+}
+
+void vending(int amount) {
+    if (amount <= 5) {
+        printf("Nothing.\n");
+    } else {
+        switch (amount) {
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14: printf("Yogert.\n"); break;
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+            case 19: 
+            case 20:
+            case 21: 
+            case 22: 
+            case 23:
+            case 24:
+            case 25: printf("Black tea.\n"); break;
+            default: printf("Yogert, Black tea.\n"); break;
+        }
+    }
 }
