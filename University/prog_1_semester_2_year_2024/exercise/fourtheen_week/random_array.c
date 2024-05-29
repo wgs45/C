5bb126efdcaa440aa19bbb24f4c6037521eb93b0
W45 <73011833+wgs45@users.noreply.github.com>
2024-05-29 20:26:12 +0800

    Update random_array.c

--- University/prog_1_semester_2_year_2024/exercise/fourtheen_week/random_array.c
+++ University/prog_1_semester_2_year_2024/exercise/fourtheen_week/random_array.c
@@ -6,30 +6,34 @@
 int *generate_arr(int n);
 
 int main() {
-  srand(time(NULL));
+  srand(time(NULL)); // Seed the random number generator
 
-  int n = rand() % 20 + 1; // generate number from 1 to 20
-  int *arr = generate_arr(n);
+  int n = rand() % 20 + 1; // Generate a random number from 1 to 20
+  int *arr = generate_arr(n); // Call the function to generate an array
   int i = 0;
 
+  // Print the elements of the array
   for (i = 0; i < n; i++) {
     printf("%d ", arr[i]);
   }
 
   printf("\n");
 
-  free(arr);
+  free(arr); // Free the dynamically allocated memory
 
   return 0;
 }
 
+// Function definition for generating an array
 int *generate_arr(int n) {
   int i = 0;
-  int *arr = (int *)malloc(n * sizeof(int));
+  int *arr = (int *)malloc(n * sizeof(int)); // Allocate memory for the array
 
+  // Fill the array with random values
   for (i = 0; i < n; i++) {
     arr[i] = rand() % 40;
   }
 
-  return arr;
+  return arr; // Return a pointer to the allocated memory
 }
+
